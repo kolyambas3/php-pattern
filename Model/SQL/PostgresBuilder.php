298e8156e635f13d6cc050b31b0d5e2da41b0c3f
kolyambas3 <shagaevkolya9@gmail.com>
2022-11-21 18:48:50 +0300

    Builder

---
+++ Model/SQL/PostgresBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Model\SQL;
+
+use App\Interface\SQLBuilder;
+
+class PostgresBuilder implements SQLBuilder
+{
+    protected $query;
+
+    protected function reset(): void
+    {
+        $this->query = new \stdClass();
+    }
+
+    public function limit(int $start, int $offset): SQLBuilder
+    {
+        parent::limit($start, $offset);
+
+        $this->query->limit = " LIMIT " . $start . " OFFSET " . $offset;
+
+        return $this;
+    }
+
+    public function where(string $field, string $value, string $operator = '='): SQLBuilder
+    {
+        if (!in_array($this->query->type, ['select', 'update', 'delete'])) {
+            throw new \Exception("WHERE can only be added to SELECT, UPDATE OR DELETE");
+        }
+        $this->query->where[] = "$field $operator '$value'";
+
+        return $this;
+    }
+
+    public function getSQL(): string
+    {
+        $query = $this->query;
+        $sql = $query->base;
+        if (!empty($query->where)) {
+            $sql .= " WHERE " . implode(' AND ', $query->where);
+        }
+        if (isset($query->limit)) {
+            $sql .= $query->limit;
+        }
+        $sql .= ";";
+        return $sql;
+    }
+
+    public function select(string $table, array $fields): SQLBuilder
+    {
+        $this->reset();
+        $this->query->base = "SELECT " . implode(", ", $fields) . " FROM " . $table;
+        $this->query->type = 'select';
+
+        return $this;
+    }
+}
